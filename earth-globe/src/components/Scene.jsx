cc2577bec7eb85fd7c1d34db5cf4dc177c918f25
Ibrahim Majid <i.majidbashir@gmail.com>
2026-03-28 23:07:42 -0400

    Enable auto-rotation in Scene component

--- earth-globe/src/components/Scene.jsx
+++ earth-globe/src/components/Scene.jsx
@@ -54,7 +54,7 @@ export default function Scene({ points, colorMap, visibleGroups, searchId, onHov
         maxDistance={8}
         zoomSpeed={0.8}
         rotateSpeed={0.5}
-        autoRotate={false}  // ← stops when a point is hovered
+        autoRotate={true}  // ← stops when a point is hovered
         autoRotateSpeed={0.4}
       />
     </Canvas>
